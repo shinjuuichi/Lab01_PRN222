47787e45c8249681371cfe333742945774aef73a
Shinjuuichi <sinlthai570@gmail.com>
2025-05-07 17:05:29 +0700

    nothing

--- ProductManagementMVC/Program.cs
+++ ProductManagementMVC/Program.cs
@@ -42,8 +42,8 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStaticFiles();
 app.UseRouting();
-app.UseSession();
 app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
